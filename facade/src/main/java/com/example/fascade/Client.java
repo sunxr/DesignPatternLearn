bbc2b44abb8e322938cd985fe667a83de1d27b1b
sunxr <969406418@qq.com>
2021-09-09 18:24:42 +0800

    外观模式

---
+++ facade/src/main/java/com/example/fascade/Client.java
@@ -0,0 +1,12 @@
+package com.example.fascade;
+
+public class Client {
+
+    public static void main(String args[]) {
+        GeneralSwitchFacade generalSwitchFacade = new GeneralSwitchFacade();
+        generalSwitchFacade.on();
+        System.out.println("----------");
+        generalSwitchFacade.off();
+    }
+
+}
---
+++ facade/src/main/java/com/example/fascade/Fan.java
@@ -0,0 +1,13 @@
+package com.example.fascade;
+
+public class Fan {
+
+    public void on() {
+        System.out.println("风扇打开");
+    }
+
+    public void off() {
+        System.out.println("风扇关闭");
+    }
+
+}
