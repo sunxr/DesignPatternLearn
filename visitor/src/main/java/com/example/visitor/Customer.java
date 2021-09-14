52566374e7dbc6786088f361eacce4c08699c183
sunxr <969406418@qq.com>
2021-09-14 14:32:17 +0800

    访问者模式

---
+++ visitor/src/main/java/com/example/visitor/Customer.java
@@ -0,0 +1,13 @@
+package com.example.visitor;
+
+public class Customer extends Visitor{
+    @Override
+    public void visit(Apple apple) {
+        System.out.println("顾客" + name + "选苹果");
+    }
+
+    @Override
+    public void visit(Book book) {
+        System.out.println("顾客" + name + "选书");
+    }
+}
---
+++ visitor/src/main/java/com/example/visitor/Visitor.java
@@ -0,0 +1,15 @@
+package com.example.visitor;
+
+public abstract class Visitor {
+
+    protected String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract void visit(Apple apple);
+
+    public abstract void visit(Book book);
+
+}
