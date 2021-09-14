f11050633a60c5bb5214c9359f8d95f062282f73
sunxr <969406418@qq.com>
2021-09-14 12:45:20 +0800

    状态模式

---
+++ state/src/main/java/com/example/state/ForumAccount.java
@@ -0,0 +1,43 @@
+package com.example.state;
+
+public class ForumAccount {
+
+    private AbstractState state;
+
+    private String name;
+
+    public ForumAccount(String name) {
+        this.name = name;
+        this.state = new PrimaryState(this);
+        System.out.println(this.name + "注册成功！");
+        System.out.println("--------------------");
+    }
+
+    public AbstractState getState() {
+        return state;
+    }
+
+    public void setState(AbstractState state) {
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void downloadFile(int score) {
+        state.downloadFile(score);
+    }
+
+    public void writeNote(int score) {
+        state.writeNote(score);
+    }
+
+    public void replyNote(int score) {
+        state.replyNote(score);
+    }
+}
