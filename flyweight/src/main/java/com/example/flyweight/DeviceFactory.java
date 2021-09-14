16ea9c48790f10fb8acf9e17de57d9f3e829dd39
sunxr <969406418@qq.com>
2021-09-14 10:46:07 +0800

    享元模式

---
+++ flyweight/src/main/java/com/example/flyweight/DeviceFactory.java
@@ -0,0 +1,39 @@
+package com.example.flyweight;
+
+import java.util.ArrayList;
+
+public class DeviceFactory {
+
+    private ArrayList<NetworkDevice> devices = new ArrayList<>();
+    private int totalTerminal = 0;
+
+    public DeviceFactory() {
+        NetworkDevice nd1 = new Switch("Cisco-WS-C2950-24");
+        devices.add(nd1);
+        NetworkDevice nd2 = new Hub("TP-LINK-HF8M");
+        devices.add(nd2);
+    }
+
+    public NetworkDevice getNetworkDevice(String type) {
+        if (type.equalsIgnoreCase("cisco")) {
+            totalTerminal++;
+            return devices.get(0);
+        }
+        else if (type.equalsIgnoreCase("tp")) {
+            totalTerminal++;
+            return devices.get(1);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public int getTotalTerminal() {
+        return totalTerminal;
+    }
+
+    public int getTotalDevice() {
+        return devices.size();
+    }
+
+}
